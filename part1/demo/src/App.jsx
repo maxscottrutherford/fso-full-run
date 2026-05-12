bf8987c2c994a68501ecfdce6abfee0b1cc030e5
Max Rutherford <maxscottrutherford@gmail.com>
2026-05-12 15:59:57 -0700

    create 1.6 exercise project

--- part1/demo/src/App.jsx
+++ part1/demo/src/App.jsx
@@ -1,70 +1,38 @@
-const Hello = (props) => {
-  return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
-    </div>
-  );
-};
+import { useState } from "react";
 
 const App = () => {
-  // const now = new Date()
-  // const a = 10
-  // const b = 20
-  // console.log(now, a + b)
-  const name = "Peter";
-  const age = 28;
-  const friends = [
-    { name: "Jeff", age: 26 },
-    { name: "Harold", age: 29 },
-  ];
-  const friendNames = ["Maya", "Jane"];
+  // const [left, setLeft] = useState(0);
+  // const [right, setRight] = useState(0);
+
+  const [clicks, setClicks] = useState({
+    left: 0,
+    right: 0
+  });
+
+  const handleLeftClick = () => {
+    const newClicks = {
+      ...clicks,
+      left: clicks.left + 1
+    }
+    setClicks(newClicks)
+  }
+
+  const handleRightClick = () => {
+    const newClicks = {
+      ...clicks,
+      right: clicks.right + 1
+    }
+    setClicks(newClicks)
+  }
+
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="Max" age={24} />
-      <Hello name={name} age={age} />
-      <p>
-        My friend {friends[0].name} is {friends[0].age}
-      </p>
-      <p>
-        My friend {friends[1].name} is {friends[1].age}
-      </p>
-      <p>{friendNames.join(", ")}</p>
+      {clicks.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {clicks.right}
     </div>
   );
 };
-
-const t = [1, 2, 3, 4, 5];
-
-const [first, second, ...rest] = t;
-
-console.log(first, second);
-console.log(rest);
-
-const object1 = {
-  name: "Max Rutherford",
-  age: 25,
-  'secret num': 1234
-};
-
-object1["secret number"] = 12334;
-
-console.log(object1);
-
-// const t = [1, -1, 3]
-// const m1 = t.map(value => value * 2)
-// console.log(t)
-// console.log(m1)
-// const t2 = t.concat(5)
-// console.log(t)
-// console.log(t2)
-// t.push(5)
-// console.log(t.length)
-// console.log(t[1])
-// t.forEach(value => {
-//   console.log(value)
-// })
 
 export default App;

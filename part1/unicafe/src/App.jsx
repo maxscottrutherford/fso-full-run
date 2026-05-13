861bd265ddc9d45d5a35355b36223030064f4255
Max Rutherford <maxscottrutherford@gmail.com>
2026-05-12 20:39:02 -0700

    exercises 1.6 to 1.12

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,23 +1,107 @@
-import { useState } from 'react'
+import { useState } from "react";
+
+const StatisticsLine = ({ stat, text }) => {
+  if (text == "Positive") {
+    return (
+      <tr>
+        <td>{text}: </td>
+        <td>{stat} %</td>
+      </tr>
+    );
+  } else {
+    return (
+      <tr>
+        <td>{text}: </td>
+        <td>{stat}</td>
+      </tr>
+    );
+  }
+};
+
+const Statistics = ({ good, neutral, bad, all }) => {
+  if (all == 0) {
+    return (
+      <div>
+        <h1>Statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>Statistics</h1>
+        <table>
+          <tbody>
+            <StatisticsLine text={"Good"} stat={good} />
+            <StatisticsLine text={"Neutral"} stat={neutral} />
+            <StatisticsLine text={"Bad"} stat={bad} />
+            <StatisticsLine text={"All"} stat={all} />
+            <StatisticsLine text={"Average"} stat={(good - bad) / all} />
+            <StatisticsLine text={"Positive"} stat={(good / all) * 100} />
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+};
+
+const Button = ({ clickHandler, text }) => {
+  return <button onClick={clickHandler}>{text}</button>;
+};
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [votes, setVotes] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+    all: 0,
+  });
+
+  const handleGoodVote = () => {
+    const newVotes = {
+      ...votes,
+      good: votes.good + 1,
+      all: votes.all + 1,
+    };
+    setVotes(newVotes);
+  };
+
+  const handleNeutralVote = () => {
+    const newVotes = {
+      ...votes,
+      neutral: votes.neutral + 1,
+      all: votes.all + 1,
+    };
+    setVotes(newVotes);
+  };
+
+  const handleBadVote = () => {
+    const newVotes = {
+      ...votes,
+      bad: votes.bad + 1,
+      all: votes.all + 1,
+    };
+    setVotes(newVotes);
+  };
+
+  console.log(votes.all);
 
   return (
     <div>
       <h1>Give Feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
-      <h1>Statistics</h1>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-    </div>
-  )
-}
+      <Button clickHandler={handleGoodVote} text={"Good"} />
+      <Button clickHandler={handleNeutralVote} text={"Neutral"} />
+      <Button clickHandler={handleBadVote} text={"Bad"} />
 
-export default App
+      <Statistics
+        good={votes.good}
+        neutral={votes.neutral}
+        bad={votes.bad}
+        all={votes.all}
+      />
+    </div>
+  );
+};
+
+export default App;

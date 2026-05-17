8b1dbbf46ebbe24b2a43e4304e6731bdcab920cb
Max Rutherford <maxscottrutherford@gmail.com>
2026-05-16 23:02:18 -0700

    clean up response data code and add error catching to importance function

--- part2/demo/src/App.jsx
+++ part2/demo/src/App.jsx
@@ -8,8 +8,8 @@ const App = () => {
   const [showAll, setShowAll] = useState(true);
 
   useEffect(() => {
-    noteService.getAll().then((response) => {
-      setNotes(response.data);
+    noteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes);
     });
   }, []);
 
@@ -22,8 +22,8 @@ const App = () => {
       id: String(notes.length + 1),
     };
 
-    noteService.create(noteObject).then((response) => {
-      setNotes(notes.concat(response.data));
+    noteService.create(noteObject).then(returnedNote => {
+      setNotes(notes.concat(returnedNote));
       setNewNote("");
     });
   };
@@ -32,9 +32,18 @@ const App = () => {
     const note = notes.find((n) => n.id === id);
     const changedNote = { ...note, important: !note.important };
 
-    noteService.update(id, changedNote).then(response => {
-      setNotes(notes.map(note => note.id === id ? response.data : note))
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {setNotes(notes.map((note) => 
+        (note.id === id ? returnedNote : note)))
     })
+    .catch(error => {
+      alert(
+        `The note '${note.content}' was already deleted from server, ${error}`
+      )
+      setNotes(notes.filter(n => n.id !== id))
+    })
+    
   };
 
   const handleNoteChange = (event) => {
